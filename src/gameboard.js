518016e73c01e5e8e83536124da3b36527ca3036
MihailPetkov96 <mpetkov228@gmail.com>
2024-01-23 23:12:48 +0200

    Initialize board as 2d array

--- src/gameboard.js
+++ src/gameboard.js
@@ -2,11 +2,13 @@ function Gameboard() {
     const board = [];
     
     for (let i = 0; i < 10; i++) {
-        
+        board.push(new Array(10).fill(0));
     }
 
 
-    return {};
+    return {
+        board
+    };
 }
 
 export default Gameboard;
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -4,3 +4,6 @@ test('Test gameboard object creation', () => {
     expect(Gameboard()).toMatchObject({});
 });
 
+test('Test gameboard board property', () => {
+    expect(Gameboard().board[0]).toEqual([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+});
